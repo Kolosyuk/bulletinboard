5135dad6e638511d5cd7d08e49ea18bbcd7890a3
Kolosyuk <crplutt@gmail.com>
2023-11-14 13:34:46 +0300

    add tests for CardComponent

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting} 
+import { provideHttpClientTesting} 
     from '@angular/common/http/testing';
 
 import { CardComponent } from './card.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Advert } from 'src/app/model/advert.interface';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -13,25 +15,62 @@ describe('CardComponent', () => {
   beforeEach(async () => {
     const advertServiceSpy = jasmine.createSpyObj('AdvertsService', ['getAdvertById']);
     const userServiceSpy = jasmine.createSpyObj('UserService', ['getCurrentUser']);
+    const mockData: Advert = {
+      id: 123,
+      user: {
+        adverts: [],
+        id: '',
+        name: '',
+        registeredTime: '',
+        role: ''
+      },
+      name: '',
+      description: '',
+      isActive: false,
+      imagesIds: ['1234-1234'],
+      cost: 0,
+      email: '',
+      phone: '',
+      location: '',
+      createdAt: '',
+      category: {
+        id: '',
+        parentId: '',
+        name: '',
+        childs: []
+      }
+    }
 
     await TestBed.configureTestingModule({
       imports: [
         CardComponent,
-        RouterTestingModule
       ],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        {provide: ActivatedRoute,
+        useValue: {
+          params: of({id: 123})
+        }}
         
       ],
     }).compileComponents();
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.data = mockData
     fixture.detectChanges();
   })
   ;
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set src to link', () => {
+    expect(component.link).toEqual(`/advert/123`);
+  });
+
+  it('should set src to main image', () => {
+    expect(component.mainImgSrc).toEqual(`http://194.87.237.48:5000/images/1234-1234`);
   });
 });
